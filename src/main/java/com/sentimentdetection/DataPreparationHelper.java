ba2cb4890bfaa40bfd610cf2ae993e08a7c83872
kwaikar <kanchan@rolloutaec.com>
2016-03-30 23:45:02 -0500

    first checkin - adding code for creating input

---
+++ src/main/java/com/sentimentdetection/DataPreparationHelper.java
@@ -0,0 +1,118 @@
+package com.sentimentdetection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+
+import com.sentimentdetection.common.Document;
+
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+/**
+ * MCAP Logistic Regression algorithm with L2 regularization
+ * 
+ * This class is implementation of Text Classifier that uses Logistic Regression
+ * approach. This approach involves calculation of P(y|x) based on trained
+ * model. We use gradient ascent approach in order to reach the global maxima.
+ * 
+ * @author Kanchan Waikar Date Created : Mar 15, 2016 - 6:44:13 PM
+ *
+ */
+public class DataPreparationHelper {
+	private static final String NOUN_PREFIX = "NN";
+	private static final String ADJECTIVE_PREFIX = "JJ";
+	private static final String VERB_PREFIX = "VB";
+	private static final String NON_ALPHABET_CHARS = "[^\\dA-Za-z ]";
+	private static final String SIMPLE_TOKENIZER_REGEX = "( |\t|\r|\n|\f)";
+	private Set<String> stopWords = new HashSet<String>();
+	MaxentTagger tagger = null;
+
+	/**
+	 * This method initializes classifier with an optional stopwords file
+	 * 
+	 * @param stopWordsFile
+	 * @throws IOException
+	 */
+	private void initialize(String stopWordsFile) throws IOException {
+		InputStream ioStream = getClass().getClassLoader().getResourceAsStream(stopWordsFile);
+		String stopWordString = IOUtils.toString(ioStream);
+		stopWords = new HashSet<String>();
+		for (String word : stopWordString.split("[\r|\n]")) {
+			if (word.trim().length() > 1) {
+				stopWords.add(word.trim().toLowerCase());
+			}
+		}
+
+		tagger = new MaxentTagger(this.getClass()
+				.getResource("/resources/POS_Tagged_Model/english-bidirectional-distsim.tagger").getFile());
+	}
+
+	/**
+	 * This method accepts Document and sets numStopWords in the same.
+	 * 
+	 * @param featureDoc
+	 * @return
+	 */
+	public void setNumStopWords(Document featureDoc) {
+		int numStopWordsFound = 0;
+		for (String word : stopWords) {
+			if (stopWords.contains(word.toLowerCase())) {
+				numStopWordsFound++;
+			}
+		}
+		featureDoc.setNumStopWordsCount(numStopWordsFound);
+	}
+
+	/**
+	 * This method sets NLP features of the document in the POJO.
+	 * 
+	 * @param featuresDoc
+	 */
+	public void setNLPFeatures(Document featuresDoc) {
+
+		int numNounEntities = 0;
+		int numVerbs = 0;
+		int numAdjectives = 0;
+		
+		for (String sentence : featuresDoc.getInputDoc()) {
+			List<HasWord> sent = Sentence.toWordList(sentence.split(" "));
+			List<TaggedWord> taggedSent = tagger.tagSentence(sent);
+			for (TaggedWord tw : taggedSent) {
+				String word = tw.word().replaceAll(NON_ALPHABET_CHARS, "");
+				if (StringUtils.isNotBlank(word)) {
+					if (!stopWords.contains(word.toLowerCase()) && word.trim().length() > 1) {
+						if (tw.tag().startsWith(NOUN_PREFIX)) {
+							numNounEntities++;
+						} else if (tw.tag().startsWith(ADJECTIVE_PREFIX)) {
+							numAdjectives++;
+						} else if (tw.tag().startsWith(VERB_PREFIX)) {
+							numVerbs++;
+						}
+					}
+				}
+			}
+		}	
+		
+		featuresDoc.setNumAdjectives(numAdjectives);
+		featuresDoc.setNumNouns(numNounEntities);
+		featuresDoc.setNumVerbs(numVerbs);
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		String stopWordsFilePrefix = "stopwords.txt";
+
+		DataPreparationHelper helper = new DataPreparationHelper();
+		helper.initialize(stopWordsFilePrefix);
+		System.out.println(helper.stopWords.size());
+	}
+}
---
+++ src/main/java/com/sentimentdetection/common/Document.java
@@ -0,0 +1,151 @@
+package com.sentimentdetection.common;
+
+import java.util.Arrays;
+
+public class Document {
+
+	private String[] inputDoc;
+	private int numStopWordsCount = 0;
+
+	private int numWords = 0;
+
+	private double positiveWordProportion;
+	private double negativeWordProportion;
+	private int numNouns;
+	private int numAdjectives;
+	private int numVerbs;
+
+	/**
+	 * @return the numVerbs
+	 */
+	public int getNumVerbs() {
+		return numVerbs;
+	}
+
+	/**
+	 * @param numVerbs the numVerbs to set
+	 */
+	public void setNumVerbs(int numVerbs) {
+		this.numVerbs = numVerbs;
+	}
+
+	/**
+	 * @return the inputDoc
+	 */
+	public String[] getInputDoc() {
+		return inputDoc;
+	}
+
+	/**
+	 * @param inputDoc
+	 *            the inputDoc to set
+	 */
+	public void setInputDoc(String[] inputDoc) {
+		this.inputDoc = inputDoc;
+		this.numWords = inputDoc.length;
+	}
+
+	/**
+	 * @return the numStopWordsCount
+	 */
+	public int getNumStopWordsCount() {
+		return numStopWordsCount;
+	}
+
+	/**
+	 * @param numStopWordsCount
+	 *            the numStopWordsCount to set
+	 */
+	public void setNumStopWordsCount(int numStopWordsCount) {
+		this.numStopWordsCount = numStopWordsCount;
+	}
+
+	/**
+	 * @return the numWords
+	 */
+	public int getNumWords() {
+		return numWords;
+	}
+
+	/**
+	 * @param numWords
+	 *            the numWords to set
+	 */
+	public void setNumWords(int numWords) {
+		this.numWords = numWords;
+	}
+
+	/**
+	 * @return the positiveWordProportion
+	 */
+	public double getPositiveWordProportion() {
+		return positiveWordProportion;
+	}
+
+	/**
+	 * @param positiveWordProportion
+	 *            the positiveWordProportion to set
+	 */
+	public void setPositiveWordProportion(double positiveWordProportion) {
+		this.positiveWordProportion = positiveWordProportion;
+	}
+
+	/**
+	 * @return the negativeWordProportion
+	 */
+	public double getNegativeWordProportion() {
+		return negativeWordProportion;
+	}
+
+	/**
+	 * @param negativeWordProportion
+	 *            the negativeWordProportion to set
+	 */
+	public void setNegativeWordProportion(double negativeWordProportion) {
+		this.negativeWordProportion = negativeWordProportion;
+	}
+
+	/**
+	 * @return the numNouns
+	 */
+	public int getNumNouns() {
+		return numNouns;
+	}
+
+	/**
+	 * @param numNouns
+	 *            the numNouns to set
+	 */
+	public void setNumNouns(int numNouns) {
+		this.numNouns = numNouns;
+	}
+
+	/**
+	 * @return the numAdjectives
+	 */
+	public int getNumAdjectives() {
+		return numAdjectives;
+	}
+
+	/**
+	 * @param numAdjectives
+	 *            the numAdjectives to set
+	 */
+	public void setNumAdjectives(int numAdjectives) {
+		this.numAdjectives = numAdjectives;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Features [inputDoc=" + Arrays.toString(inputDoc) + ", numStopWordsCount=" + numStopWordsCount
+				+ ", numWords=" + numWords + ", positiveWordProportion=" + positiveWordProportion
+				+ ", negativeWordProportion=" + negativeWordProportion + ", numNouns=" + numNouns + ", numAdjectives="
+				+ numAdjectives + "]";
+	}
+
+}
