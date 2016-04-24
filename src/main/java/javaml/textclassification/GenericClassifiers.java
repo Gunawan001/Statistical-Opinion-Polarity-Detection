b8db546ddd9c8f9449596f42e2df0fab364d0a8d
kwaikar <kanchan@rolloutaec.com>
2016-04-24 16:10:06 -0500

    adding data

--- src/main/java/javaml/textclassification/GenericClassifiers.java
+++ src/main/java/javaml/textclassification/GenericClassifiers.java
@@ -1,63 +1,102 @@
 package javaml.textclassification;
 
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Random;
 
 import libsvm.LibSVM;
 import net.sf.javaml.classification.Classifier;
+import net.sf.javaml.classification.KDtreeKNN;
+import net.sf.javaml.classification.KNearestNeighbors;
+import net.sf.javaml.classification.evaluation.CrossValidation;
+import net.sf.javaml.classification.evaluation.PerformanceMeasure;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
 import net.sf.javaml.tools.data.ARFFHandler;
+import net.sf.javaml.tools.weka.WekaClassifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.Logistic;
 
- 
 public class GenericClassifiers {
-    /**
-     * Shows the default usage of the KNN algorithm.
-     */
-    public static void main(String[] args)throws Exception {
-    	
+	/**
+	 * Shows the default usage of the KNN algorithm.
+	 */
+	public static void main(String[] args) throws Exception {
 
-    	Dataset trainingData = ARFFHandler.loadARFF(new File(args[0]), 7);
-        /*
-         * Contruct a KNN classifier that uses 5 neighbors to make a decision.
-         */
-        Classifier knn = new LibSVM();
-        knn.buildClassifier(trainingData);
+		/*
+		 * Contruct a KNN classifier that uses 5 neighbors to make a decision.
+		 */
+		String trainingDataFilePath = args[0];
+		String testDataFilePath = args[1];
 
-        /*
-         * Load a data set for evaluation, this can be a different one, but we
-         * will use the same one.
-         */
-       
-        Dataset testData = ARFFHandler.loadARFF(new File( args[1]), 7);
-        /* Counters for correct and wrong predictions. */
-       double accurcy= predictAndReturnAccuracy(knn, trainingData);
-       System.out.println("Training Data" +accurcy);
-         accurcy= predictAndReturnAccuracy(knn, testData);
-       System.out.println("Training Data" +accurcy);
-    }
+		Logistic wekaClassifier = new Logistic();
+		Classifier wekaClassifierWrapper = new WekaClassifier(wekaClassifier);
+		TrainModelAndPrintAccuracyForTrainingAndTestingDataSet(trainingDataFilePath, testDataFilePath,
+				wekaClassifierWrapper);
+
+		NaiveBayes naiveClassifier = new NaiveBayes();
+		wekaClassifierWrapper = new WekaClassifier(naiveClassifier);
+		TrainModelAndPrintAccuracyForTrainingAndTestingDataSet(trainingDataFilePath, testDataFilePath,
+				wekaClassifierWrapper);
+
+		Classifier classifier = new KDtreeKNN(75);
+		TrainModelAndPrintAccuracyForTrainingAndTestingDataSet(trainingDataFilePath, testDataFilePath, classifier);
+
+		classifier = new KNearestNeighbors(75);
+		TrainModelAndPrintAccuracyForTrainingAndTestingDataSet(trainingDataFilePath, testDataFilePath, classifier);
+
+		classifier = new LibSVM();
+		TrainModelAndPrintAccuracyForTrainingAndTestingDataSet(trainingDataFilePath, testDataFilePath, classifier);
+
+	}
+
+	/**
+	 * This method trains model and prints accuracy for both data sets.
+	 * 
+	 * @param trainingDataFilePath
+	 * @param testDataFilePath
+	 * @param classifier
+	 * @throws FileNotFoundException
+	 */
+	public static void TrainModelAndPrintAccuracyForTrainingAndTestingDataSet(String trainingDataFilePath,
+			String testDataFilePath, Classifier classifier) throws FileNotFoundException {
+		Dataset trainingData = ARFFHandler.loadARFF(new File(trainingDataFilePath), 7);
+		classifier.buildClassifier(trainingData);
+		Dataset testData = ARFFHandler.loadARFF(new File(testDataFilePath), 7);
+
+		double accurcy = predictAndReturnAccuracy(classifier, trainingData);
+		System.out.println("Training Data Accuracy:" + accurcy);
+		accurcy = predictAndReturnAccuracy(classifier, testData);
+		System.out.println("Test Data Accuracy:" + accurcy);
+	}
 
 	/**
 	 * @param knn
 	 * @param dataForClassification
 	 */
 	public static double predictAndReturnAccuracy(Classifier knn, Dataset dataForClassification) {
-		int correct = 0, wrong = 0;
-        /* Classify all instances and check with the correct class values */
-        for (Instance inst : dataForClassification) {
-            Object predictedClassValue = knn.classify(inst);
-            Object realClassValue = inst.classValue();
-        //    System.out.println(predictedClassValue+ " : "+ realClassValue);
-            if (predictedClassValue.equals(realClassValue))
-                correct++;
-            else
-                wrong++;
-        }
-        System.out.println("Correct predictions  " + correct);
-        System.out.println("Wrong predictions " + wrong);
-        double accuracy =(double)100*correct/(correct+wrong);
-        System.out.println("Accuracy: "+(double)100*correct/(correct+wrong));
-        return accuracy;
+		int wrongClassifications = 0;
+		for (Instance inst : dataForClassification) {
+
+			if (!inst.classValue().equals(knn.classify(inst))) {
+				wrongClassifications++;
+			}
+		}
+		System.out.println("Correct predictions  " + (dataForClassification.size() - wrongClassifications));
+		System.out.println("Wrong predictions " + wrongClassifications);
+
+		double accuracy = (double) 100 * (dataForClassification.size() - wrongClassifications)
+				/ dataForClassification.size();
+		System.out.println("Accuracy: "
+				+ (double) 100 * (dataForClassification.size() - wrongClassifications) / dataForClassification.size());
+
+		CrossValidation cv = new CrossValidation(knn);
+		Map<Object, PerformanceMeasure> p = cv.crossValidation(dataForClassification, 5, new Random(1));
+		System.out.println("Performance Measure");
+		System.out.println(p);
+
+		return accuracy;
 	}
 
 }
